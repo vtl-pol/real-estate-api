421d852a36c56eb054741d75bb1dc0f71bd83d76
Vitalii <vitalii.zdanovskyi@gmail.com>
2020-06-15 11:25:58 +0300

    EsLint fix

--- migrations/20200615074029_fixDefaultValues.js
+++ migrations/20200615074029_fixDefaultValues.js
@@ -1,12 +1,12 @@
 
 const up = function (knex) {
-  return knex.schema.alterTable("properties", function (t) {
+  return knex.schema.alterTable('properties', function (t) {
     t.integer('propertyStatus').default(1).unsigned().alter()
   })
 }
 
 const down = function (knex) {
-  return knex.schema.alterTable("properties", function (t) {
+  return knex.schema.alterTable('properties', function (t) {
     t.integer('propertyStatus').default(0).unsigned().alter()
   })
 }
